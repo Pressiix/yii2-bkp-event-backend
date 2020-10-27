06804875a87a1558034e5c1e3d717b50c298e631
Watcharaphon Piamphuetna <watcharaphonp@bangkokpost.co.th>
2020-10-27 17:03:07 +0700

    ...

--- config/db.php
+++ config/db.php
@@ -8,9 +8,9 @@ return [
     // 'charset' => 'utf8',
 
     'class' => 'yii\db\Connection',
-    'dsn' => !strpos($_SERVER['REQUEST_URI'],'heroku') ? 'mysql:host=127.0.0.1;dbname=postdigital_ptd' : 'mysql:host=us-cdbr-east-02.cleardb.com;dbname=heroku_d18bfef1a27cdf7',
-    'username' => !strpos($_SERVER['REQUEST_URI'],'heroku') ? 'postdigital_ptd' : 'b3036e28d41b04',
-    'password' => !strpos($_SERVER['REQUEST_URI'],'heroku') ? 'Thahxay8' : '97b98c1c',
+    'dsn' => 'mysql:host=us-cdbr-east-02.cleardb.com;dbname=heroku_d18bfef1a27cdf7',
+    'username' => 'b3036e28d41b04',
+    'password' => '97b98c1c',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
--- config/web.php
+++ config/web.php
@@ -49,20 +49,7 @@ $config = [
                 ],
             ],
         ],
-        // 'db' => $db,
-        'db' => [
-        
-            'class' => 'yii\db\Connection',
-            'dsn' => !strpos($_SERVER['REQUEST_URI'],'heroku') ? 'mysql:host=127.0.0.1;dbname=postdigital_ptd' : 'mysql:host=us-cdbr-east-02.cleardb.com;dbname=heroku_d18bfef1a27cdf7',
-            'username' => !strpos($_SERVER['REQUEST_URI'],'heroku') ? 'postdigital_ptd' : 'b3036e28d41b04',
-            'password' => !strpos($_SERVER['REQUEST_URI'],'heroku') ? 'Thahxay8' : '97b98c1c',
-            'charset' => 'utf8',
-        
-            // Schema cache options (for production environment)
-            //'enableSchemaCache' => true,
-            //'schemaCacheDuration' => 60,
-            //'schemaCache' => 'cache',
-        ],
+        'db' => $db,
         
         'urlManager' => [
             'enablePrettyUrl' => true,
