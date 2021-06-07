98304a930292dae9c59234ad863f59bec3a2f919
Watcharaphon Piamphuetna <watcharapon.piam@gmail.com>
2021-06-07 19:19:20 +0700

    Update db.php

--- config/db.php
+++ config/db.php
@@ -1,16 +1,10 @@
 <?php
 
 return [
-    // 'class' => 'yii\db\Connection',
-    // 'dsn' => 'mysql:host=127.0.0.1;dbname=postdigital_ptd',
-    // 'username' => 'postdigital_ptd',
-    // 'password' => 'Thahxay8',
-    // 'charset' => 'utf8',
-
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=us-cdbr-east-02.cleardb.com;dbname=heroku_d18bfef1a27cdf7',
-    'username' => 'b3036e28d41b04',
-    'password' => '97b98c1c',
+    'dsn' => 'mysql:host=xxxxx;dbname=xxxxxx',
+    'username' => 'xxxxx',
+    'password' => 'xxxxx',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
